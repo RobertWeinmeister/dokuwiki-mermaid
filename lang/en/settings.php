8499ae12bc7f889b973b33a61d44a8bdaee9d8a5
robert <develop@weinmeister.org>
2023-04-05 13:43:55 +0200

    Update to support Mermaid version 10.1 remotely and 9.4.3 locally

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,8 +1,9 @@
 <?php
 
 $lang['location'] = 'Use locally or remotely hosted mermaid file?';
-$lang['location_o_local'] = 'locally hosted, version 9.4 (CJS)';
+$lang['location_o_local'] = 'locally hosted, version 9.4.3 (CJS)';
 $lang['location_o_latest'] = 'remotely hosted, latest version (ESM)';
-$lang['location_o_remote100'] = 'remotely hosted, version 10.0 (EMS)';
+$lang['location_o_remote101'] = 'remotely hosted, version 10.1 (ESM)';
+$lang['location_o_remote100'] = 'remotely hosted, version 10.0 (ESM)';
 $lang['location_o_remote94'] = 'remotely hosted, version 9.4 (CJS)';
 $lang['location_o_remote93'] = 'remotely hosted, version 9.3 (CJS)';
