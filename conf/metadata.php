8eaa3f3bfb7521f1a8041100b4fb5e8685d7b160
Robert Weinmeister <develop@weinmeister.org>
2023-09-04 18:54:44 +0200

    Update to support Mermaid version 10.4 remotely and locally

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,3 +1,3 @@
 <?php
 
-$meta['location']  = array('multichoice', '_choices' => array('local', 'latest', 'remote103', 'remote102', 'remote101', 'remote100', 'remote94', 'remote93'));
+$meta['location']  = array('multichoice', '_choices' => array('local', 'latest', 'remote104', 'remote103', 'remote102', 'remote101', 'remote100', 'remote94', 'remote93'));
