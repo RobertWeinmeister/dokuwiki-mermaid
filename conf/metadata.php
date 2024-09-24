234919efa8005beed10dc6dc3cf60395f015ba42
robert <develop@weinmeister.org>
2024-09-24 17:06:17 +0200

    Update to support Mermaid version 11.2.1 remotely and locally
    - Reduces the available options for the Mermaid version

--- conf/metadata.php
+++ conf/metadata.php
@@ -1,4 +1,4 @@
 <?php
 
-$meta['location'] = array('multichoice', '_choices' => array('local', 'latest', 'remote108', 'remote106', 'remote104', 'remote103', 'remote102', 'remote101', 'remote100', 'remote94', 'remote93'));
+$meta['location'] = array('multichoice', '_choices' => array('local', 'latest', 'remote1091', 'remote943'));
 $meta['theme'] = array('multichoice', '_choices' => array('default', 'neutral', 'dark', 'forest', 'base'));
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,8 +1,12 @@
 <?php
 
 $lang['location'] = 'Use locally or remotely hosted mermaid file?';
-$lang['location_o_local'] = 'locally hosted, version 10.8.0 (CJS)';
+$lang['location_o_local'] = 'locally hosted, version 11.2.1 (CJS)';
 $lang['location_o_latest'] = 'remotely hosted, latest version (ESM)';
+$lang['location_o_remote943'] = 'remotely hosted, version 9.4.3 (CJS)';
+$lang['location_o_remote1091'] = 'remotely hosted, version 10.9.1 (ESM)';
+
+// old options, no longer explicitly available
 $lang['location_o_remote108'] = 'remotely hosted, version 10.8 (ESM)';
 $lang['location_o_remote106'] = 'remotely hosted, version 10.6 (ESM)';
 $lang['location_o_remote104'] = 'remotely hosted, version 10.4 (ESM)';
@@ -12,6 +16,7 @@ $lang['location_o_remote101'] = 'remotely hosted, version 10.1 (ESM)';
 $lang['location_o_remote100'] = 'remotely hosted, version 10.0 (ESM)';
 $lang['location_o_remote94'] = 'remotely hosted, version 9.4 (CJS)';
 $lang['location_o_remote93'] = 'remotely hosted, version 9.3 (CJS)';
+
 $lang['theme'] = 'Default theme';
 $lang['theme_o_default'] = 'default';
 $lang['theme_o_neutral'] = 'neutral';
