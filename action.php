98dbd122fca82d70012849b42939e9672c817b59
robert <develop@weinmeister.org>
2024-10-11 17:58:51 +0200

    WIP2

--- action.php
+++ action.php
@@ -10,14 +10,10 @@ if (!defined('DOKU_INC')) die();
 
 class action_plugin_mermaid extends \dokuwiki\Extension\ActionPlugin
 {
-    private $containsMermaid;
-    private $test;
-
     /** @inheritDoc */
     public function register(Doku_Event_Handler $controller)
     {
         $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'load');
-        $controller->register_hook('TPL_ACT_PREPROCESS', 'BEFORE', $this, 'check');
         $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'handleAjaxRequest');
     }
 
@@ -87,20 +83,9 @@ class action_plugin_mermaid extends \dokuwiki\Extension\ActionPlugin
         exit();
     }
 
-    public function check(Doku_Event $event, $param)
-    {
-        echo '<pre>' , var_export($event) , '</pre>';
-        echo '<pre>' , var_export($param) , '</pre>';
-        $this->test = $event;
-        //$this->containsMermaid = strpos($event->data, '<mermaid') !== false;
-    }
-
     public function load(Doku_Event $event, $param)
     {
-        echo '<pre>' , var_export($this->test) , '</pre>';
-        echo '<pre>' , var_export($this->containsMermaid) , '</pre>';
-
-        if(!is_null($this->containsMermaid)  && !$this->containsMermaid)
+        if(strpos(rawWiki($ID), '<mermaid') === false)
         {
             return;
         }
