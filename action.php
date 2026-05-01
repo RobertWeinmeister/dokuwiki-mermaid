6787c0030f7ee41dcdb472d10236861d21e312ca
Martin Lormes <1294964+tfnab@users.noreply.github.com>
2026-05-02 00:34:29 +0200

    remove the quick and dirty bit of the fix for https://github.com/RobertWeinmeister/dokuwiki-mermaid/issues/54

--- action.php
+++ action.php
@@ -172,9 +172,6 @@ class action_plugin_mermaid extends \dokuwiki\Extension\ActionPlugin
             return true;
         }
 
-        global $ACT;
-        if ('preview'==$ACT) return true;
-
         return false;
     }
 
